edc842cd2e734f2a2a151bd45b6a6b00d37bd785
Bruno Oliveira <up202208700@edu.fe.up.pt>
2025-04-17 12:25:56 +0100

    Implement protocol handler

--- src/protocol/ProtocolHandler.java
+++ src/protocol/ProtocolHandler.java
@@ -1,5 +1,26 @@
 package protocol;
 
-public interface ProtocolHandler {
-    
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+public class ProtocolHandler {
+    private Map<String, Function<TokenList, Boolean>> handlers;
+
+    public ProtocolHandler() {
+        this.handlers = new HashMap<>();
+    }
+
+    public void addHandler(String command, Function<TokenList, Boolean> handler) {
+        handlers.put(command, handler);
+    }
+
+    private Boolean defaultHandler(TokenList list) {
+        return false;
+    }
+
+    public boolean handle(TokenList list) {
+        var handler = handlers.get(list.getCommand());
+        return handler != null ? handler.apply(list) : defaultHandler(list);
+    }
 }
