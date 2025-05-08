3284d436f43045527be0b373dd8b276d0d47fb3b
Bruno Oliveira <up202208700@edu.fe.up.pt>
2025-05-08 19:35:06 +0100

    Add reconnection with exponential backoff

--- src/protocol/ProtocolPort.java
+++ src/protocol/ProtocolPort.java
@@ -2,11 +2,17 @@ package protocol;
 
 import java.io.IOException;
 
+import exception.EndpointUnreachableException;
 import protocol.unit.ProtocolUnit;
 
 public interface ProtocolPort {
     void send(ProtocolUnit unit) throws IOException;
+
     ProtocolUnit receive() throws IOException;
+
+    void reconnect() throws EndpointUnreachableException, IOException;
+
+    boolean isClosed();
 
     void close() throws IOException;
 }
