1e348084b46459d0a48c02128ddfdbe066244508
Bruno Oliveira <up202208700@edu.fe.up.pt>
2025-05-09 00:24:12 +0100

    Fix shellcheck

--- src/gen-cert.sh
+++ src/gen-cert.sh
@@ -9,17 +9,17 @@ SERVER_PASSWORD=$(openssl rand -base64 32)
 CLIENT_PASSWORD=$(openssl rand -base64 32)
 
 BUILD_DIR="$(dirname "$0")/build"
-KEYSTORE="server.keystore"
-TRUSTSTORE="client.truststore"
-CERT="server.crt"
-SERVER_PASSWORD_FILE="server-pass.txt"
-CLIENT_PASSWORD_FILE="client-pass.txt"
+KEYSTORE='server.keystore'
+TRUSTSTORE='client.truststore'
+CERT='server.crt'
+SERVER_PASSWORD_FILE='server-pass.txt'
+CLIENT_PASSWORD_FILE='client-pass.txt'
 
 rm -f "$KEYSTORE" "$TRUSTSTORE" "$CERT" server-pass.txt client-pass.txt
 cd "$BUILD_DIR" || exit 1
 
 # Generate the server keystore
-keytool -genkeypair -alias server -keyalg RSA -keystore "$KEYSTORE" -storepass "$SERVER_PASSWORD" -keypass "$SERVER_PASSWORD" -dname "CN=cpd, OU=fe, O=up, L=Porto, S=Porto, C=Portugal" || exit 1
+keytool -genkeypair -alias server -keyalg RSA -keystore "$KEYSTORE" -storepass "$SERVER_PASSWORD" -keypass "$SERVER_PASSWORD" -dname 'CN=cpd, OU=fe, O=up, L=Porto, S=Porto, C=Portugal' || exit 1
 
 # Export the server certificate
 keytool -export -alias server -keystore "$KEYSTORE" -file "$CERT" -storepass "$SERVER_PASSWORD" || exit 1
@@ -28,8 +28,8 @@ keytool -export -alias server -keystore "$KEYSTORE" -file "$CERT" -storepass "$S
 keytool -import -alias server -file "$CERT" -keystore "$TRUSTSTORE" -storepass "$CLIENT_PASSWORD" -noprompt || exit 1
 
 # Write passwords to files
-echo -n "$SERVER_PASSWORD" > "$SERVER_PASSWORD_FILE"
-echo -n "$CLIENT_PASSWORD" > "$CLIENT_PASSWORD_FILE"
+printf '%s' "$SERVER_PASSWORD" > "$SERVER_PASSWORD_FILE"
+printf '%s' "$CLIENT_PASSWORD" > "$CLIENT_PASSWORD_FILE"
 
 echo
 echo "Keystore and truststore generated successfully."
